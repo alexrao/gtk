3ba414daba2d0a1cf78f3e5e14ed794946af9f53
alexrao <rxp2013@163.com>
2016-11-02 15:56:04 +0800

    添加gtk2的中的表格布局使用

--- example/layer/main.c
+++ example/layer/main.c
@@ -2,9 +2,10 @@
 #include "print.h"
 
 
-void show_button(GtkWidget *window, GtkWidget **fix)
+#if 0
+void show_button(GtkWidget *window)
 {
-    GtkWidget *fixed = *fix;
+    GtkWidget *fixed = NULL;
     GtkWidget *button1, *button2, *button3;
 
     fixed = gtk_fixed_new();
@@ -24,12 +25,13 @@ void show_button(GtkWidget *window, GtkWidget **fix)
 
 }
 
-void show_vbox(GtkWidget *window, GtkWidget **box)
+// 竖着一排按钮
+void show_vbox(GtkWidget *window)
 {
-    GtkWidget *vbox = *box;
+    GtkWidget *vbox = NULL;
     GtkWidget *button1, *button2, *button3, *button4, *button5;
 
-    vbox = gtk_box_new(TRUE, 1);
+    vbox = gtk_box_new(TRUE, 2);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
     button1 = gtk_button_new_with_label("Settings");
@@ -44,11 +46,44 @@ void show_vbox(GtkWidget *window, GtkWidget **box)
     gtk_box_pack_start(GTK_BOX(vbox), button4, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), button5, TRUE, TRUE, 0);
 }
+#endif
+
+void show_table(GtkWidget *window)
+{
+    GtkWidget *table;
+    GtkWidget *button;
+
+    char *value[16] =
+    {
+        "7", "8", "9", "/",
+        "4", "5", "6", "*",
+        "1", "2", "3", "+",
+        "0", ".", "=", "-",
+    };
+
+    table = gtk_table_new(4, 4, TRUE);
+    gtk_table_set_row_spacings(GTK_TABLE(table), 2);
+    gtk_table_set_col_spacings(GTK_TABLE(table), 2);
+
+    int i=0, j=0;
+    int pos = 0;
+
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            button = gtk_button_new_with_label(value[4*i+j]);
+            gtk_table_attach_defaults(GTK_TABLE(table), button, j, j+1, i, i+1);
+        }
+    }
+
+    gtk_container_add(GTK_CONTAINER(window), table);
+
+}
 
 int main(int argc, char **argv)
 {
     GtkWidget *window = NULL;
-    GtkWidget *fixed = NULL;
     GtkWidget *button1, *button2, *button3;
 
     gtk_init(&argc, &argv);
@@ -57,14 +92,15 @@ int main(int argc, char **argv)
     gtk_window_set_title(GTK_WINDOW(window), "Layer"); // 设置窗口的标题
     gtk_window_set_default_size(GTK_WINDOW(window), 640, 360); // 设置窗口的大小
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); // 设置窗口的位置
-
-    //show_button(window, &fixed);
-    show_vbox(window, &fixed);
-
-    gtk_widget_show_all(window);
-
     g_signal_connect_swapped(G_OBJECT(window), "destroy",
             G_CALLBACK(gtk_main_quit), NULL);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 5);
+
+    //show_button(window);
+    //show_vbox(window);
+    show_table(window);
+
+    gtk_widget_show_all(window);
 
     gtk_main(); // 主事件循环
 
