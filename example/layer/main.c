6b77c4516c2641026cdc31fee86054a09081f051
alexrao <rxp2013@163.com>
2016-11-02 15:30:45 +0800

    添加竖直的按钮

--- example/layer/main.c
+++ example/layer/main.c
@@ -1,18 +1,11 @@
 #include <gtk/gtk.h>
 #include "print.h"
 
-int main(int argc, char **argv)
+
+void show_button(GtkWidget *window, GtkWidget **fix)
 {
-    GtkWidget *window = NULL;
-    GtkWidget *fixed = NULL;
+    GtkWidget *fixed = *fix;
     GtkWidget *button1, *button2, *button3;
-
-    gtk_init(&argc, &argv);
-
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL); // 创建一个窗口
-    gtk_window_set_title(GTK_WINDOW(window), "Layer"); // 设置窗口的标题
-    gtk_window_set_default_size(GTK_WINDOW(window), 640, 360); // 设置窗口的大小
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); // 设置窗口的位置
 
     fixed = gtk_fixed_new();
     gtk_container_add(GTK_CONTAINER(window), fixed);
@@ -28,6 +21,45 @@ int main(int argc, char **argv)
     button3 = gtk_button_new_with_label("Button");
     gtk_fixed_put(GTK_FIXED(fixed), button3, 100, 250);
     gtk_widget_set_size_request(button3, 100, 50);
+
+}
+
+void show_vbox(GtkWidget *window, GtkWidget **box)
+{
+    GtkWidget *vbox = *box;
+    GtkWidget *button1, *button2, *button3, *button4, *button5;
+
+    vbox = gtk_box_new(TRUE, 1);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    button1 = gtk_button_new_with_label("Settings");
+    button2 = gtk_button_new_with_label("Accoounts");
+    button3 = gtk_button_new_with_label("Loans");
+    button4 = gtk_button_new_with_label("Cash");
+    button5 = gtk_button_new_with_label("Debts");
+
+    gtk_box_pack_start(GTK_BOX(vbox), button1, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button2, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button3, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button4, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button5, TRUE, TRUE, 0);
+}
+
+int main(int argc, char **argv)
+{
+    GtkWidget *window = NULL;
+    GtkWidget *fixed = NULL;
+    GtkWidget *button1, *button2, *button3;
+
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL); // 创建一个窗口
+    gtk_window_set_title(GTK_WINDOW(window), "Layer"); // 设置窗口的标题
+    gtk_window_set_default_size(GTK_WINDOW(window), 640, 360); // 设置窗口的大小
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER); // 设置窗口的位置
+
+    //show_button(window, &fixed);
+    show_vbox(window, &fixed);
 
     gtk_widget_show_all(window);
 
