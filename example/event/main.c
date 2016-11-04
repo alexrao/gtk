cc710183219c34e0dc5d2802a014c9781048c52b
alexrao <rxp2013@163.com>
2016-11-04 20:19:30 +0800

    添加定时器的功能

--- example/event/main.c
+++ example/event/main.c
@@ -26,11 +26,29 @@ void enter_button(GtkWidget *widget, gpointer data)
     gtk_widget_modify_bg(widget, GTK_STATE_PRELIGHT, &color);
 }
 
+gboolean on_button_press (GtkWidget* widget,
+          GdkEventButton * event, GdkWindowEdge edge)
+{
+    if (event->type == GDK_BUTTON_PRESS)
+    {
+        if (event->button == 1)
+        {
+            print("button[%d], x=%d, y=%d", (int)event->button, (int)event->x_root, (int)event->y_root);
+            gtk_window_begin_move_drag(GTK_WINDOW(gtk_widget_get_toplevel(widget)),
+                    event->button,
+                    event->x_root,
+                    event->y_root,
+                    event->time);
+        }
+    }
+
+    return FALSE;
+}
+
+
 int main(int argc, char *argv[])
 {
   GtkWidget *window;
-  GtkWidget *button;
-  GtkWidget *fixed;
 
   gtk_init(&argc, &argv);
 
@@ -38,8 +56,10 @@ int main(int argc, char *argv[])
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
   gtk_window_set_default_size(GTK_WINDOW(window), 300, 300);
   gtk_window_set_title(GTK_WINDOW(window), "Simple");
-  gtk_widget_add_events(GTK_WIDGET(window), GDK_CONFIGURE);
+  //gtk_window_set_decorated(GTK_WINDOW(window), FALSE); // 设置为无边框
+  gtk_widget_add_events(GTK_WIDGET(window), GDK_BUTTON_PRESS_MASK);
 
+  /*
   fixed = gtk_fixed_new();
   gtk_container_add(GTK_CONTAINER(window), fixed);
 
@@ -49,14 +69,16 @@ int main(int argc, char *argv[])
 
   g_signal_connect(G_OBJECT(button), "enter",
           G_CALLBACK(enter_button), NULL);
+*/
+
+  g_signal_connect(G_OBJECT(window), "button_press_event",
+          G_CALLBACK(on_button_press), NULL);
 
   g_signal_connect_swapped(G_OBJECT(window), "destroy",
                   G_CALLBACK(gtk_main_quit), G_OBJECT(window));
 
-  g_signal_connect(G_OBJECT(window), "configure-event",
-                  G_CALLBACK(frame_callback), NULL);
-
-  
+  //g_signal_connect(G_OBJECT(window), "configure-event",
+  //                G_CALLBACK(frame_callback), NULL);
 
   gtk_widget_show_all(window);
   gtk_main();
